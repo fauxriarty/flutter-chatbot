86a06de244aff73c4d3b4c0753da3f3a91984a23
fauxriarty <aditya.chheda04@gmail.com>
2023-10-20 23:27:42 +0530

    made login page a lil better

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,7 +14,15 @@ class LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
 
+  bool isLoading = false;
+  String? errorMessage;
+
   Future<User?> signInWithGoogle() async {
+    setState(() {
+      isLoading = true;
+      errorMessage = null;
+    });
+
     try {
       final GoogleSignInAccount? googleSignInAccount =
           await googleSignIn.signIn();
@@ -31,9 +39,16 @@ class LoginPageState extends State<LoginPage> {
         return userCredential.user;
       }
     } catch (error) {
+      setState(() {
+        errorMessage = "Error logging in. Please try again.";
+      });
       // ignore: avoid_print
       print(error);
       return null;
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
     return null;
   }
@@ -42,32 +57,57 @@ class LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Page'),
+        title: const Text(
+          'Login Page',
+          style: TextStyle(color: Color.fromARGB(255, 13, 68, 37)),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Login with Google",
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text("Sign In"),
-              onPressed: () async {
-                User? user = await signInWithGoogle();
-                if (user != null) {
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => HomePage(user: user)),
-                  );
-                }
-              },
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (isLoading)
+                const CircularProgressIndicator()
+              else
+                const Icon(Icons.login,
+                    size: 80, color: Color.fromARGB(255, 53, 136, 74)),
+              const SizedBox(height: 20),
+              const Text(
+                "Login with Google",
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(iconSize: MaterialStateProperty.all(30)),
+                child: const Text(
+                  "Sign In",
+                  style: TextStyle(
+                      fontSize: 16, color: Color.fromARGB(255, 46, 84, 16)),
+                ),
+                onPressed: () async {
+                  User? user = await signInWithGoogle();
+                  if (user != null) {
+                    // ignore: use_build_context_synchronously
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(user: user)),
+                    );
+                  }
+                },
+              ),
+              if (errorMessage != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    errorMessage!,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
